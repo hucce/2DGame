2edb88e614c0394ad9379003ba353a3b8ab8ad77
hucce <huccce@gmail.com>
2021-05-06 15:14:50 +0900

    첫번째 수업 커밋

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 3f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float playerSpeed = h * moveSpeed * Time.deltaTime;
+        Vector3 vector3 = new Vector3();
+        vector3.x = playerSpeed;
+        transform.Translate(vector3);
+
+        if (h < 0)
+        {
+            GetComponent<Animator>().SetBool("Walk", true);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (h == 0)
+        {
+            GetComponent<Animator>().SetBool("Walk", false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Walk", true);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
